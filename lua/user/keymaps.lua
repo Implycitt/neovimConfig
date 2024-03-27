7d95d3549a6968881c8ea316ec0482a17c73e4ec
Implycitt <qgbordelon@gmail.com>
2024-03-26 22:02:08 -0500

    Small change to keymaps to navigate splits easier

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -80,3 +80,17 @@ vim.opt.guicursor = {
 	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
 }
 
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap=true, silent=true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Move around splits using Ctrl + {h,j,k,l}
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
+
